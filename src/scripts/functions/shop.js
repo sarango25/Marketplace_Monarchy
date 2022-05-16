de05da0008a6f2357fc7a7158f006479b92ae1b9
Sebastian Arango <sebastianarango@MacBook-Pro-de-Sebastian.local>
2022-05-16 06:09:12 -0500

    view products registered

--- src/scripts/functions/shop.js
+++ src/scripts/functions/shop.js
@@ -1,7 +1,10 @@
-import {db} from "./properties"
+import {db,storage} from "./properties"
 import{getProducts} from "../functions/products";
 
 const productSection = document.getElementById("products");
+const categoryFilter = document.getElementById("category");
+
+let products = [];
 
 async function loadProducts(){
     const firebaseProducts = await getProducts(db);
@@ -9,9 +12,10 @@ async function loadProducts(){
     firebaseProducts.forEach (product => {
         renderProducts(product);
     });
-    console.log(firebaseProducts);
-}
 
+    products.firebaseProducts;
+    
+}
 
 function renderProducts(item){
     const product = document.createElement("a");
@@ -19,11 +23,12 @@ function renderProducts(item){
 
     product.setAttribute("href",`./product.html?id=abcdef`)
 
-    const coverImage = item.images ? item.images[0]: "./img/placeholder.jpg"
+    const coverImage = item.images ? item.images[0]: "./img/placeholder.jpg";
 
     product.innerHTML = ` <a class="product" href="#">
     <img src="${coverImage}" alt="" class="product__image">
     <div class="product__info">
+        <p class="product__category">${item.category}</p>
         <h2 class="product__name">${item.name}</h2>
         <h3 class="product__price">${item.price}</h3>
         <button class="product__btn">Añadir al carrito</button>
@@ -33,4 +38,21 @@ function renderProducts(item){
 
 productSection.appendChild(product);
 }
+
+function filterBy(){
+    const newCategory = categoryFilter.value;
+    const filteredProducts = products.filter((product)=>product.category === newCategory);
+
+    productSection.innerHTML ="";
+
+    filteredProducts.forEach (product => {
+        renderProducts(product);
+    });
+    
+}
+
+categoryFilter.addEventListener("change",e => {
+    filterBy();
+})
+
 loadProducts();
